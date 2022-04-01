e8adb8e60dfd8c83b37b064c48ae50b77eca788a
deblur99 <deblur99@kakao.com>
2022-04-01 20:50:17 +0900

    added-4-1

--- BOJ/sorting/03_10989_수_정렬하기_3.cpp
+++ BOJ/sorting/03_10989_수_정렬하기_3.cpp
@@ -1,61 +1,34 @@
 #include <iostream>
-#include <vector>
 
 #define ARR_LENGTH 10000000
 #define BUF_LENGTH 1000000
 #define MAX_VAL    10000
 
-template <typename T>
-void counting(std::vector<T> &arr) {
-    std::vector<T> itemArr;
-    itemArr.reserve(MAX_VAL + 1);
-    std::fill(itemArr.begin(), itemArr.end(), -1);
-
-    std::vector<T> countArr;
-    countArr.reserve(MAX_VAL + 1);
-    std::fill(countArr.begin(), countArr.end(), -1);
-
-    auto iter = arr.begin();
-    auto countIter = countArr.begin();
-
-    for (; iter != arr.end(); iter++) {
-        countArr[*iter]++;
-    }
-
-    for (int i = 0; i < MAX_VAL + 1; i++) {
-        if (countArr[i] != -1) {
-            while (countArr[i] > 0) {
-                std::cout << i << "\n";
-                countArr[i]--;
-            }
-        }
-    }
-}
+int countArr[MAX_VAL + 1] = { 0,  };
 
 int main() {
-    std::cin.tie(NULL); std::cin.sync_with_stdio(false);
+    std::cin.tie(NULL); 
+    std::cin.sync_with_stdio(false);
+    std::cout.sync_with_stdio(false);
 
     int n, item;
-    std::vector<int> arr;
 
     std::cin >> n;
 
-    if (n < 1 || n > ARR_LENGTH) return 0;
-
-    arr.reserve(BUF_LENGTH);
-
     for (int i = 0; i < n; i++) {
-        if (arr.size() < BUF_LENGTH) {
-            std::cin >> item;
-            arr.push_back(item);
-        } else {
-            counting(arr);
-            arr.clear();
-        }
-    }
+        std::cin >> item;
 
-    counting(arr);
-    arr.clear();
+        countArr[item]++;
+    }
+    
+    // 4MB 제한 (int * 100만) 걸려 있으므로 별도의 배열에 저장하지 않고 바로 출력해야 함
+    for (int i = 1; i <= MAX_VAL; i++) {
+        if (countArr[i] > 0) {
+            for (int j = 0; j < countArr[i]; j++) {
+                std::cout << i << "\n";
+            }
+        }            
+    }
 
     return 0;
 }
