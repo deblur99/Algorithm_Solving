8679577c944e70b0edca3e0cdfadfe33f7541b2e
deblur99 <deblur99@kakao.com>
2022-04-30 21:09:57 +0900

    added-4-30

---
+++ BOJ/week3-ds/07_2346_풍선_터트리기.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+#include <list>
+
+class Balloon {
+private:
+    int _number;
+    int _nextNumber;
+public:
+    Balloon(int number, int nextNumber) {
+        _number = number;
+        _nextNumber = nextNumber;
+    }
+
+    int getNextNumber() {
+        return _nextNumber;
+    }
+};
+
+static std::list<Balloon> balloonList;
+static std::queue<Balloon> balloonResultQueue;
+
+int main() {
+    std::cin.tie(NULL);
+    std::cin.sync_with_stdio(false); std::cout.sync_with_stdio(false);
+
+    int n;
+    std::cin >> n;
+    if (n < 1 || n > 1000) return 0;
+        
+    int nextNumber = 0;
+    for (int i = 0; i < n; i++) {
+        std::cin >> nextNumber;
+        if (nextNumber == 0) return 0;
+        balloonList.push_back(Balloon(i+1, nextNumber));
+    }
+
+    for (int i = 0; i < balloonList.size(); i++) {
+        balloonList.
+    }
+
+    return 0;
+}
